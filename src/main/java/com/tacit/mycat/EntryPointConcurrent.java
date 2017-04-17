6a405a7d627cb3c34dc52d69407d71d7b930a59b
Paul Hemmings <phemmings@l-c02md1jnfd57.wsgc.com>
2017-04-17 11:48:26 -0700

    collect items before closing thread. otherwise thread may refuse requests and cause error

--- src/main/java/com/tacit/mycat/EntryPointConcurrent.java
+++ src/main/java/com/tacit/mycat/EntryPointConcurrent.java
@@ -11,10 +11,7 @@ import org.apache.http.util.EntityUtils;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.Executor;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -24,57 +21,58 @@ public class EntryPointConcurrent {
 
     public static void main(String[] args) {
         get("/images/:filter", "text/html", (request, response) -> {
-            return buildResponse(request.params(":filter"));
+            return new EntryPointConcurrent().buildResponse(request.params(":filter"));
         });
     }
 
-    private static List<String> buildResponse(String request) {
+    private List<String> buildResponse(String request) throws InterruptedException {
 
         // split filter into stream of tags
         Stream<String> filters = buildTagStream(request);
 
         // create a flickr URL for each tag
-        List<String> urls = filters.map(EntryPointConcurrent::buildUrl).collect(Collectors.toList());
+        List<String> urls = filters.map(this::buildUrl).collect(Collectors.toList());
 
         // build an executor for each url
         ExecutorService executor = getExecutor(urls.size());
 
         // make concurrent requests for all those urls
-        Stream<TagItemsResponse> taggedItems = requestTaggedItems(urls, executor);
+        List<TagItemsResponse> taggedItems = requestTaggedItems(urls, executor);
 
         // show down the pool (otherwise threads may be leaked when executor drops out of scope)
         executor.shutdown();
 
         // create a stream of elements for each of those tagged items
-        Stream<String> elements = taggedItems.map(EntryPointConcurrent::buildElementStream).flatMap(x -> x);
+        Stream<String> elements = taggedItems.stream().map(this::buildElementStream).flatMap(x -> x);
 
         // return as a list
         return elements.collect(Collectors.toList());
     }
 
-    private static Stream<String> buildTagStream(String filter) {
+    private Stream<String> buildTagStream(String filter) {
         return Arrays.asList(filter.split(",")).stream();
     }
 
-    private static String buildUrl(String tag) {
+    private String buildUrl(String tag) {
         return "http://api.flickr.com/services/feeds/photos_public.gne?tags=" + tag + "&format=json&jsoncallback=?";
     }
 
-    private static String buildElement(TagItemsResponse.Items items) {
+    private String buildElement(TagItemsResponse.Items items) {
         return "<img src='" + items.media.m + "' />";
     }
 
-    private static Stream<String> buildElementStream(TagItemsResponse tagItemsResponse) {
-        return Arrays.asList(tagItemsResponse.items).stream().map(EntryPointConcurrent::buildElement);
+    private Stream<String> buildElementStream(TagItemsResponse tagItemsResponse) {
+        return Arrays.asList(tagItemsResponse.items).stream().map(this::buildElement);
     }
 
-    private static Stream<TagItemsResponse> requestTaggedItems(List<String> urls, ExecutorService executor) {
+    private List<TagItemsResponse> requestTaggedItems(List<String> urls, ExecutorService executor) {
         return urls.stream()
                 .map(url -> CompletableFuture.supplyAsync(() -> callUrl(url), executor))
-                .map(CompletableFuture::join);
+                .map(CompletableFuture::join)
+                .collect(Collectors.toList());
     }
 
-    private static ExecutorService getExecutor(int size) {
+    private ExecutorService getExecutor(int size) {
 
         return Executors.newFixedThreadPool(Math.min(size * 2, 100), runnable -> {
             Thread thread = new Thread(runnable);
@@ -83,7 +81,7 @@ public class EntryPointConcurrent {
         });
     }
 
-    private static TagItemsResponse callUrl(String url) {
+    private TagItemsResponse callUrl(String url) {
         try {
             final HttpUriRequest httpGet = new HttpGet(url);
             HttpClient httpClient = HttpClients.createDefault();
@@ -91,7 +89,7 @@ public class EntryPointConcurrent {
             String responseContent = EntityUtils.toString(response.getEntity());
             responseContent = responseContent.substring(1, responseContent.length()-1);
             return new Gson().fromJson(responseContent, TagItemsResponse.class);
-        } catch (IOException e) {
+        } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
         return null;
